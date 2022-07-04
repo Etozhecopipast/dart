b223e3dac83fb81fd22b2d64172b8b3e81cddc6d
Etozhecopipast <75393629+Etozhecopipast@users.noreply.github.com>
2022-07-04 20:01:37 +0300

    Create miniCalc.dart

---
+++ miniCalc.dart
@@ -0,0 +1,22 @@
+import "dart:io";
+
+void main() {
+  print("Welcome to the calculator!");
+  print("Enter first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+  print("Enter second number:");
+  double num2 = double.parse(stdin.readLineSync()!);
+  print("Enter a type of operation");
+  String operation = stdin.readLineSync()!;
+  
+  if (operation == "+"){
+     print(num1+num2);
+  }
+  else if (operation == "-"){
+     print(num1-num2);
+  }
+  else if (operation == "*"){
+     print(num1*num2);
+  }
+  else print(num1/num2);
+}
