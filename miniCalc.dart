9062aca6bab5119263ce8328271c8559b3d20cda
Etozhecopipast <75393629+Etozhecopipast@users.noreply.github.com>
2022-07-04 20:59:04 +0300

    Update miniCalc.dart

--- miniCalc.dart
+++ miniCalc.dart
@@ -2,10 +2,13 @@ import "dart:io";
 
 void main() {
   print("Welcome to the calculator!");
+  
   print("Enter first number:");
   double num1 = double.parse(stdin.readLineSync()!);
+  
   print("Enter second number:");
   double num2 = double.parse(stdin.readLineSync()!);
+  
   print("Enter a type of operation");
   String operation = stdin.readLineSync()!;
   
