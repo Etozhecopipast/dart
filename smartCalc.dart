a6ac5dd3ec28e4987cc2beb6ad706ae7dc16095e
Etozhecopipast <75393629+Etozhecopipast@users.noreply.github.com>
2022-07-05 09:33:18 +0300

    Create smartCalc.dart

---
+++ smartCalc.dart
@@ -0,0 +1,37 @@
+import "dart:io";
+
+String prompt (String promptText){
+  
+  print ("${promptText}: ");
+  String answer = stdin.readLineSync()!;
+  return answer;
+}
+
+double promptDouble(){
+  
+  print("Enter a number:");
+  double myNum = double.parse(stdin.readLineSync()!);
+  return myNum;
+}
+
+void main() {
+
+  print("Smart calculator");
+  
+  double num1 = promptDouble();
+  double num2 = promptDouble();
+  String op = prompt("Enter an operatinon (+,-,/,*):");
+
+  if(op == '+'){
+    print(num1 + num2);
+  } else if (op == '-'){
+    print(num1 - num2);
+  }else if (op == '/'){
+    print(num1/num2);
+  }else if (op == '*') {
+    (print(num1 * num2));
+  } else {
+    print("Invalid operator!");
+  }
+  
+}
