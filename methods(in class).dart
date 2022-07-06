24f9e59a819aa610b37be42ec63efaf6ca6d9992
Etozhecopipast <75393629+Etozhecopipast@users.noreply.github.com>
2022-07-06 22:20:16 +0300

    Create methods(in class).dart

---
+++ methods(in class).dart
@@ -0,0 +1,22 @@
+class Student{ 
+  String name = '';
+  double gpa = 0;
+
+  Student(String name, double gpa){
+    this.name = name;
+    this.gpa = gpa;
+  }
+  
+  //Метод - функция, которая находится внутри класса
+  bool exelStudent(){
+    return this.gpa >= 4.5;
+  }
+}
+
+void main() {
+
+  Student Ilya = Student('Ilya', 4.5);
+  Student Alex = Student ('Alex', 3.3);
+
+  print(Alex.exelStudent());
+}
