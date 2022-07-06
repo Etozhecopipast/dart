8dcd8906be5af54610d76a8dc57d6edf8035da77
Etozhecopipast <75393629+Etozhecopipast@users.noreply.github.com>
2022-07-06 21:17:40 +0300

    Update for(forin).dart

--- for(forin).dart
+++ for(forin).dart
@@ -1,19 +1,5 @@
 import "dart:io";
 
-String prompt (String promptText){
-  
-  print (promptText);
-  String answer = stdin.readLineSync()!;
-  return answer;
-}
-
-double promptDouble(){
-  
-  print("Enter a number:");
-  double myNum = double.parse(stdin.readLineSync()!);
-  return myNum;
-}
-
 void main() {
 
   List<String> friends =["Vlad", "Alex", "Victoria"];
